c2faabf4829e46b93d1b990633ff9b6a3bc2a35f
JOHAESEONG <whgotjd1599@gmai.com>
2020-12-06 16:42:55 +0900

    Frst Upload

---
+++ app/src/main/java/com/example/howlstagram_f16/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.howlstagram_f16
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.example.howlstagram_f16.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class LoginActivity : AppCompatActivity() {
+    var auth: FirebaseAuth? = null
+    var emailEditText: EditText? = null
+    var passwordEditText: EditText? = null
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        emailEditText = findViewById(R.id.email_edittext)
+        passwordEditText = findViewById(R.id.password_edittext)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        binding.emailLoginButton.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(
+            emailEditText.toString(),
+            passwordEditText.toString()
+            //binding.emailEdittext.toString(),
+            //binding.passwordEdittext.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Creating a user account
+                    moveMainPage(task.result?.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    //Show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                    //Login if you have account
+                    signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(
+            emailEditText.toString(),
+            passwordEditText.toString()
+            //binding.emailEdittext.toString(),
+            //binding.passwordEdittext.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Login
+                } else {
+                    //Show the error message
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
