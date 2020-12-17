7e983f71e9f3f1a4f5e92cd8e09103f6f194dab0
JOHAESEONG <whgotjd1599@gmai.com>
2020-12-17 18:39:09 +0900

    NabigationBar_01

--- app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.howlstagram_f16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import com.example.howlstagram_f16.databinding.ActivityLoginBinding
+import com.example.howlstagram_f16.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                //R.id.action_home -> {}
+            }
+        }
     }
 }
