d247e434ef1ef28b4c8914874b8cb9c40fd51456
JOHAESEONG <whgotjd1599@gmai.com>
2020-12-24 13:26:24 +0900

    NavigationBar Success

--- app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_f16/MainActivity.kt
@@ -4,19 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.fragment.app.Fragment
 import com.example.howlstagram_f16.databinding.ActivityLoginBinding
 import com.example.howlstagram_f16.databinding.ActivityMainBinding
+import com.example.howlstagram_f16.navigation.AlarmFragment
+import com.example.howlstagram_f16.navigation.DetailViewFragment
+import com.example.howlstagram_f16.navigation.GridFragment
+import com.example.howlstagram_f16.navigation.UserFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.rpc.context.AttributeContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.bottomNavigation.setOnNavigationItemReselectedListener {
             when(it.itemId){
-                //R.id.action_home -> {}
+                R.id.action_home ->{
+                    val detailViewFragment = DetailViewFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                    true
+                }
+                R.id.action_search ->{
+                    val gridFragment = GridFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                    true
+                }
+                R.id.action_add_photo ->{
+                    true
+                }
+                R.id.action_favorite_alarm->{
+                    val alarmFragment = AlarmFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                    true
+                }
+                R.id.action_account -> {
+                    val userFragment = UserFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                    true
+                }
+                else -> false
             }
         }
     }
--- app/src/main/java/com/example/howlstagram_f16/navigation/UserFragment.kt
+++ app/src/main/java/com/example/howlstagram_f16/navigation/UserFragment.kt
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.howlstagram_f16.R
 
-class UserFragment: Fragment() {
+class UserFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
         return view
     }
 }
